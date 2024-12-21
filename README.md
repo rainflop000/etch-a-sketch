a200ab90634ce7e28ae2fa93f84230e281b6c06f
Billy <87596003+rainflop000@users.noreply.github.com>
2024-12-21 21:58:22 +1100

    Created grid layout

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # etch-a-sketch
+My etch-a-sketch project for The Odin Project
---
+++ javascript.js
@@ -0,0 +1,16 @@
+
+const containerDiv = document.querySelector('.div-container');
+
+for (let row = 0; row < 16; row++) {
+    const rowDiv = document.createElement('div');
+    rowDiv.classList.add('row');
+
+    for (let cell = 0; cell < 16; cell++) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        rowDiv.appendChild(cell);
+    }
+    containerDiv.appendChild(rowDiv);
+    
+}
+
