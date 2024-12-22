c2fd1c4d713fbeb6770d1412a9d33468a43e510d
Billy <87596003+rainflop000@users.noreply.github.com>
2024-12-22 14:20:27 +1100

    Finished function allowing user to enter grid size. Added effects.

--- javascript.js
+++ javascript.js
@@ -1,22 +1,39 @@
 
 const containerDiv = document.querySelector('.div-container');
+let gridSize = 16;
 
-for (let row = 0; row < 16; row++) {
-    const rowDiv = document.createElement('div');
-    rowDiv.classList.add('row');
+drawGrid(gridSize);
 
-    for (let cell = 0; cell < 16; cell++) {
-        const cell = document.createElement('div');
-        cell.classList.add('cell');
-        rowDiv.appendChild(cell);
+const sizeBtn = document.querySelector('.user-size-popup');
+sizeBtn.addEventListener('click', () => {
+    let newSize = prompt("How many squares per side would you like your grid?");
+    newSize = parseInt(newSize);
+    if (newSize && newSize > 0 && newSize <=100) {
+        containerDiv.textContent = '';
+        drawGrid(newSize);
+    } else {
+        alert('Enter a valid number between 1 and 100.');
     }
-    containerDiv.appendChild(rowDiv);
-}
-
-const cells = document.querySelectorAll('.cell');
-cells.forEach(cell => {
-    cell.addEventListener('mouseenter', () => {
-        cell.style.backgroundColor = 'gray';
 });
+
+function drawGrid(gridSize) {
+    for (let row = 0; row < gridSize; row++) {
+        const rowDiv = document.createElement('div');
+        rowDiv.classList.add('row');
+
+        for (let cell = 0; cell < gridSize; cell++) {
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            rowDiv.appendChild(cell);
+        }
+        containerDiv.appendChild(rowDiv);
+    }
+
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach(cell => {
+        cell.addEventListener('mouseenter', () => {
+            cell.style.backgroundColor = 'gray';
+    });
 }); 
 
+}
