f1039466c75ddc438ac433d657faee55b0908cef
Billy <87596003+rainflop000@users.noreply.github.com>
2025-01-01 17:38:14 +1100

    Added clear grid button. Without this change, user would need to refresh page to clear grid

--- javascript.js
+++ javascript.js
@@ -1,10 +1,10 @@
-const containerDiv = document.querySelector('.div-container');
+const containerDiv = document.querySelector('.grid-container');
 let gridSize = 16;
 let drawing = false;
 
 drawGrid(gridSize);
 
-const sizeBtn = document.querySelector('.user-size-popup');
+const sizeBtn = document.querySelector('.grid-size-btn');
 sizeBtn.addEventListener('click', () => {
     let newSize = prompt("How many squares per side would you like your grid?");
     newSize = parseInt(newSize);
@@ -14,6 +14,12 @@ sizeBtn.addEventListener('click', () => {
     } else {
         alert('Enter a valid number between 1 and 100.');
     }
+});
+
+const clearBtn = document.querySelector('.clear-btn');
+clearBtn.addEventListener('click', () => {
+    containerDiv.textContent = '';
+    drawGrid(gridSize);
 });
 
 function drawGrid(gridSize) {
@@ -31,18 +37,21 @@ function drawGrid(gridSize) {
 
     const cells = document.querySelectorAll('.cell');
 
+    // Start drawing
     cells.forEach(cell => {
         cell.addEventListener('mousedown', () => {
             drawing = true;
             cell.style.backgroundColor = 'gray';
     });
 
+    // Continue drawing if user mouse is pressed
     cell.addEventListener('mouseover', () => {
         if (drawing) {
             cell.style.backgroundColor = 'gray';
         }
     });
 
+    // Finish drawing if user releases mouse
     cell.addEventListener('mouseup', () => {
         drawing = false;
     });
